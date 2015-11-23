6092f7fd6882b53784f6d3e848cf22441586ecdd
Zelaf <zelaf@zelaf.eu>
2015-11-23 19:03:18 +0100

    main

---
+++ sh_addon.lua
@@ -0,0 +1,6 @@
+maestro.command("addons", {}, function(caller)
+	if IsValid(caller) then
+		local pl = Entity( 1 )
+		pl:SendLua( "print( 'Hello World' )" )
+			gui.OpenURL( "http://steamcommunity.com/sharedfiles/filedetails/?id=558185441" ) 
+end
